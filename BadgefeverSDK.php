b4353212060dbf94bcfa92e48bcd528f8410e6a1
Tomino <dostal.it@gmail.com>
2012-12-30 00:06:28 +0800

    refactoring and adding assign calls

--- BadgefeverSDK.php
+++ BadgefeverSDK.php
@@ -17,31 +17,98 @@ class BadgefeverSDK {
 	}
 
 	protected function request(){
+		// Additional options
+		$this->_curlOptions[CURLOPT_RETURNTRANSFER] = 1;
+
 		$c = curl_init();
 		curl_setopt_array($c, $this->_curlOptions);
 
 		$response = curl_exec($c);
 
+		curl_close($c);
+
 		return $response;
-	}
-
-	public function getBadges($email=null, $format='json', $params = array()){
-		$query = self::createHash($email).'.'.$format;
-
-		if (!empty($params)){
-			$query .= '?';
-			foreach($params as $key=>$val){
-				$query .= $key.'='.$val.'&';
-			}
-			$query = substr($query,0,-1);
-		}
-
-		$this->_curlOptions[CURLOPT_URL] = $this->_bfCalls['display_basic']['url'].$query;
-
-		$this->request();
 	}
 
 	public function createHash($email){
 		return md5( strtolower( trim($email) ) );
+	}
+
+	public function getBadges($email=null, $params = array()){
+		$query = array(
+			'email='.$this->createHash($email),
+		);
+
+		$query = array_merge($query,$this->parseParams($params));
+
+		$query = join('&',$query);
+		$this->_curlOptions[CURLOPT_URL] = $this->_endPoint.'display/?'.$query;
+
+		$response = $this->request();
+
+		return $response;
+	}
+
+	public function hasBadge($email=null, $badge=null, $params=array()){
+		$query = array(
+			'email='.$this->createHash($email),
+			'badge='.$badge,
+		);
+
+		$query = array_merge($query,$this->parseParams($params));
+
+		$query = join('&',$query);
+		$this->_curlOptions[CURLOPT_URL] = $this->_endPoint.'assign/?'.$query;
+
+		$response = $this->request();
+
+		return $response;
+	}
+
+	public function getAssignCount($badge=null, $params=array()){
+		$query = array(
+			'badge='.$badge,
+		);
+
+		$query = array_merge($query,$this->parseParams($params));
+
+		$query = join('&',$query);
+		$this->_curlOptions[CURLOPT_URL] = $this->_endPoint.'assign/?'.$query;
+
+		$response = $this->request();
+
+		return $response;
+	}
+
+	public function assignBadge($badge=null, $email=null, $params=array()){
+		$query = array(
+			'badge='.$badge,
+			'email='.$this->createHash($email),
+		);
+
+		$query = array_merge($query,$this->parseParams($params));
+
+		$this->_curlOptions[CURLOPT_URL] = $this->_endPoint.'assign';
+		$this->_curlOptions[CURLOPT_POST] = count($query);
+		$this->_curlOptions[CURLOPT_POSTFIELDS] = join('&',$query);
+
+		$response = $this->request();
+
+		return $response;
+	}
+
+	protected function parseParams($params){
+		$output = array();
+		if (!empty($params)){
+			foreach($params as $key=>$val){
+				$output[] = $key.'='.urlencode($val);
+			}
+		}
+
+		// add apiKey and apiSecret
+		$output[] = 'apiKey='.urlencode($this->_apiKey);
+		$output[] = 'apiSecret='.urlencode($this->_apiSecret);
+
+		return $output;
 	}
 }
--- test.php
+++ test.php
@@ -1,5 +1,32 @@
 <?php
 require_once 'BadgefeverSDK.php';
+$bf = new BadgefeverSDK('qwepoi61','bad70078c9c559d4dc7167b6e05d0049');
 
-$bf = new BadgefeverSDK('qwepoi61','7000671891ef69412715f88218908540');
-echo $bf->getBadges('test@badgefever.com','json',array('size'=>'medium'));
+?>
+<section>
+	<h1>Display Badges for test@badgefever.com (GET)</h1>
+	<?php
+		echo $bf->getBadges('test@badgefever.com',array('format'=>'html','size'=>'medium'));
+	?>
+</section>
+
+<section>
+	<h1>Did email test@badgefever.com achieved badge 1? (GET)</h1>
+	<?php
+		echo $bf->hasBadge('test6@badgefever.com',2);
+	?>
+</section>
+
+<section>
+    <h1>How many times has been badge 1 assigned? (GET)</h1>
+	<?php
+		echo $bf->getAssignCount(1);
+	?>
+</section>
+
+<section>
+    <h1>Assign Badge 1 to test@badgefever.com (POST)</h1>
+	<?php
+		echo $bf->assignBadge(1,'test@badgefever.com',array('level'=>1));
+	?>
+</section>
